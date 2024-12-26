9ff6e0a7ae931a89b50f25a6ad7fc4b4f7da0916
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-26 20:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // write your code here
+function range(start, end) {
+  // Create an empty array to store the result
+  let result = [];
+  
+  // Use a for loop to iterate from start to end (inclusive)
+  for (let i = start; i <= end; i++) {
+    result.push(i); // Add each integer to the result array
+  }
+  
+  // Return the resulting array
+  return result;
+}
 
 // do not change the code below.
 let start = parseInt(prompt("Enter Start of the Range."));
